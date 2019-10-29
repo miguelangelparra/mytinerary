e8cbcb32b78bfd44905cc0a490668a59b7fb65fa
miguelangelparra <miguelangel.parradavila@gmail.com>
2019-10-29 17:25:21 -0300

    modificacion

--- src/components/Carrousel4img.jsx
+++ src/components/Carrousel4img.jsx
@@ -8,9 +8,9 @@ import Container from 'react-bootstrap/Container'
 
 const Carrousel4img = (props) => {
   const image = [
-    [{ "title": "New York", "src": "../assets/img/newyork.png" }, { "title": "Barcelona", "src": "../assets/img/barcelona.png" }, { "title": "Caracas", "src": "../assets/img/caracas.png" }, { "title": "Bogota", "src": "../assets/img/bogota.png" }],
-    [{ "title": "Sao Paulo", "src": "../assets/img/saopaulo.png" }, { "title": "Cartagena", "src": "../assets/img/cartagena.png" }, { "title": "Santiago", "src": "../assets/img/santiago.png" }, { "title": "Panama", "src": "../assets/img/panama.png" }],
-    [{ "title": "Bariloche", "src": "../assets/img/bariloche.png" }, { "title": "Miami", "src": "../assets/img/miami.png" }, { "title": "Londres", "src": "../assets/img/londres.png" }, { "title": "Berlin", "src": "../assets/img/berlin.png" }]
+    { title: "New York", "src": "../assets/img/newyork.png" }, { title: "Barcelona", "src": "../assets/img/barcelona.png" }, { title: "Caracas", "src": "../assets/img/caracas.png" }, { title: "Bogota", "src": "../assets/img/bogota.png" },
+    { title: "Sao Paulo", "src": "../assets/img/saopaulo.png" }, { title: "Cartagena", "src": "../assets/img/cartagena.png" }, { title: "Santiago", "src": "../assets/img/santiago.png" }, { title: "Panama", "src": "../assets/img/panama.png" },
+    { title: "Bariloche", "src": "../assets/img/bariloche.png" }, { title: "Miami", "src": "../assets/img/miami.png" }, { title: "Londres", "src": "../assets/img/londres.png" }, { title: "Berlin", "src": "../assets/img/berlin.png" }
   ]
 
   return (
@@ -26,13 +26,16 @@ const Carrousel4img = (props) => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                 </Card.Body>
-              </Card>        </Col>
-            <Col>        <Card >
-              <Card.Img variant="top" src="holder.js/" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-              </Card.Body>
-            </Card></Col>
+              </Card>
+            </Col>
+            <Col>
+              <Card >
+                <Card.Img variant="top" src="holder.js/" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
           </Row>
           <Row>
             <Col>
@@ -41,7 +44,8 @@ const Carrousel4img = (props) => {
                 <Card.Body>
                   <Card.Title>Card Title</Card.Title>
                 </Card.Body>
-              </Card></Col>
+              </Card>
+            </Col>
             <Col>
               <Card >
                 <Card.Img variant="top" src="holder.js/" />
